f79cdfabaeb3de31cfe5cda74d69f07dc4515911
Moch Yudha Rusdian <55824602+Yudharus@users.noreply.github.com>
2019-11-08 20:37:38 +0700

    Add files via upload

---
+++ PBO1-10118040-Latihan40-(ObjectOriented)WarnaKepribadian/src/pbo1/pkg10118040/latihan40/objectoriented/warnakepribadian/PBO110118040Latihan40ObjectOrientedWarnaKepribadian.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo1.pkg10118040.latihan40.objectoriented.warnakepribadian;
+
+/**
+ *
+ * @author asus
+ * NAMA         : Moch Yudha Rusdian
+ * NIM          : 10118040
+ * Kelas        : IF-1
+ *  Deskripsi Program : Program ini untuk menampilkan tampilan dari warna dan 
+ *                     kepribadian.
+ 
+ */
+public class PBO110118040Latihan40ObjectOrientedWarnaKepribadian {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Warna skuyMu = new Warna();
+        
+        skuyMu.ioWarnaMu();
+    }
+    
+}
+    
+   
+    
+
